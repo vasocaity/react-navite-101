a069a2fc4b7b6c15b37ab87248fb331696fa9dd5
waraporn maensiri <wara.phon@odds.team>
2020-02-05 21:27:13 +0700

    void function

--- App.js
+++ App.js
@@ -7,14 +7,7 @@
  */
 
 import React, { Component } from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import {Platform, StyleSheet, Text, Button, View} from 'react-native';
 
 import {
   Header,
@@ -24,62 +17,31 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
+export function _onPressButton() {
+  alert('You tapped the button!')
+}
+
 const App = () => {
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>llllll: Hermes</Text>
-            </View>
-          )}
-          <Vara />
-          <View style={styles.body}>
-            <View style={{backgroundColor: `#111`}}>
-              <Text style={styles.sectionTitle}>Step One ,,,,</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <View style={styles.container}>
+      <View style={styles.buttonContainer}>
+        <Button onPress={_onPressButton} title="Press Me" />
+      </View>
+    </View>
   );
 };
 
 class Vara extends Component {
   render() {
-    return <Text style={{color: 'red'}}>Keep improve your slef.</Text>;
+    return <Text>Keep improve your self</Text>;
   }
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
   scrollView: {
     backgroundColor: Colors.lighter,
   },
